637ea27dbcf0713ca1c28e432037cac36955f2da
ICHINOSE Shogo <shogo82148@gmail.com>
2022-11-26 15:50:42 +0900

    --notes ""

--- publish.sh
+++ publish.sh
@@ -4,4 +4,7 @@ set -uex
 ROOT=$(git rev-parse --show-toplevel)
 VERSION=$1
 
-gh release create --title "Stawberry Perl $VERSION" "$VERSION" "$ROOT/download/strawberry-perl-$VERSION"/*
+gh release create \
+    --title "Stawberry Perl $VERSION" \
+    --notes "" \
+    "$VERSION" "$ROOT/download/strawberry-perl-$VERSION"/*
