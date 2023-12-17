3d18df9d3d5f06a24fa06a0dbad89c2812a67056
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-18 00:05:19 +0900

    download GitHub instead of strawberryperl.com

--- download.sh
+++ download.sh
@@ -6,16 +6,8 @@ VERSION=$1
 mkdir -p "$ROOT/download/strawberry-perl-$VERSION"
 cd "$ROOT/download/strawberry-perl-$VERSION"
 
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-64bit.msi"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-64bit-PDL.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-64bit-portable.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-64bit.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-32bit.msi"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-32bit-PDL.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-32bit-portable.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-$VERSION-32bit.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-no64-$VERSION-32bit-portable.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-no64-$VERSION-32bit.zip"
-curl -f --retry 3 -LO "https://strawberryperl.com/download/$VERSION/strawberry-perl-ld-$VERSION-64bit-PDL.zip" || rm -f "strawberry-perl-ld-$VERSION-64bit-PDL.zip"
+curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-PDL.zip"
+curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-portable.zip"
+curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit.msi"
 sha1sum -- *.msi *.zip > sha1sum.txt
 sha256sum -- *.msi *.zip > sha256sum.txt
