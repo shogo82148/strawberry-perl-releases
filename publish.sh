e8b2b660200811a72a79268010b1019334ee889b
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-20 23:33:11 +0900

    fix typo

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@ ROOT=$(git rev-parse --show-toplevel)
 VERSION=$1
 
 gh release create \
-    --title "Stawberry Perl $VERSION" \
+    --title "Strawberry Perl $VERSION" \
     --notes "" \
     "$VERSION" "$ROOT/download/strawberry-perl-$VERSION"/*
