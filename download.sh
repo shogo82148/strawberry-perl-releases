76f9ce4f3a4cc3b04daaec539acf9a7646dc2157
ICHINOSE Shogo <shogo82148@gmail.com>
2024-08-19 21:52:00 +0900

    update download url

--- download.sh
+++ download.sh
@@ -6,8 +6,8 @@ VERSION=$1
 mkdir -p "$ROOT/download/strawberry-perl-$VERSION"
 cd "$ROOT/download/strawberry-perl-$VERSION"
 
-curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-PDL.zip"
-curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-portable.zip"
-curl -f --retry 3 -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit.msi"
+curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit_UCRT/strawberry-perl-$VERSION-64bit-PDL.zip"
+curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit_UCRT/strawberry-perl-$VERSION-64bit-portable.zip"
+curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit_UCRT/strawberry-perl-$VERSION-64bit.msi"
 sha1sum -- *.msi *.zip > sha1sum.txt
 sha256sum -- *.msi *.zip > sha256sum.txt
