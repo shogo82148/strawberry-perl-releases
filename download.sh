de434707adfb55a2cf0c3605dae4f5d07f47501b
ICHINOSE Shogo <shogo82148@gmail.com>
2026-04-08 17:26:43 +0900

    update download script to fetch release information from GitHub

--- download.sh
+++ download.sh
@@ -3,11 +3,20 @@
 set -uex
 ROOT=$(git rev-parse --show-toplevel)
 VERSION=$1
+
+# set up
 mkdir -p "$ROOT/download/strawberry-perl-$VERSION"
 cd "$ROOT/download/strawberry-perl-$VERSION"
 
-curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-PDL.zip"
-curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit-portable.zip"
-curl -f --retry 3 -C - -LO "https://github.com/StrawberryPerl/Perl-Dist-Strawberry/releases/download/SP_${VERSION//.}_64bit/strawberry-perl-$VERSION-64bit.msi"
+# fetch the release information
+RELEASES=$(curl -f --retry 3 -sSL https://strawberryperl.com/releases.json)
+EDITION=$(echo "$RELEASES" | jq -r --arg VERSION "$VERSION" '.[] | select(.version == $VERSION) | .edition')
+
+# download the files
+curl -f --retry 3 -C - -LO "$(echo "$EDITION" | jq -r '.msi.url')"
+curl -f --retry 3 -C - -LO "$(echo "$EDITION" | jq -r '.pdl.url')"
+curl -f --retry 3 -C - -LO "$(echo "$EDITION" | jq -r '.portable.url')"
+
+# calculate checksums
 sha1sum -- *.msi *.zip > sha1sum.txt
 sha256sum -- *.msi *.zip > sha256sum.txt
